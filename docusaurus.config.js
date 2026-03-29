f2f4da610556964e47539d863990f93469d64366
Ana Beatriz Pereira <anas.bp10@gmail.com>
2026-03-29 20:29:17 -0300

    Change onBrokenLinks from 'throw' to 'warn' in docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   organizationName: 'anabe-apereira', // Usually your GitHub org/user name.
   projectName: 'doc-tech-challenge', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
